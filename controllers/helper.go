1ddbdb37ba9244b459bb3f9fd4e86afdc7c9a6ae
tomasteylor25 <tomasteylor.24@gmail.com>
2026-04-30 15:29:09 -0500

    Se crea archivo helper con funcion respondJSON para dar solucion a error de declaracion múltiples veces del mismo paquete

---
+++ controllers/helper.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(payload)
+}
