7b868bed4b3df8cc046c5a88b0fbe02b795ee4c7
tomasteylor25 <tomasteylor.24@gmail.com>
2026-04-30 16:26:17 -0500

    Se agrega a carpeta models archivo recuperacion_contraseña con su modelo

---
+++ models/recuperacion_contrasena.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type RecuperacionContrasena struct {
+	IDRecuperacion    int        `json:"id_recuperacion"`
+	IDUsuario         int        `json:"id_usuario"`
+	Token             string     `json:"token"`
+	Codigo            *string    `json:"codigo"`
+	Usado             bool       `json:"usado"`
+	FechaExpiracion   time.Time  `json:"fecha_expiracion"`
+	Activo            bool       `json:"activo"`
+	FechaCreacion     time.Time  `json:"fecha_creacion"`
+	FechaModificacion time.Time  `json:"fecha_modificacion"`
+}
